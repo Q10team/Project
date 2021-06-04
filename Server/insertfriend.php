68fa8e790cb1dcfae75553c4eb1163d542901b05
황규현 <57509844+hgyuhyeon@users.noreply.github.com>
2021-06-04 21:17:31 +0900

    Update insertfriend.php

--- Server/insertfriend.php
+++ Server/insertfriend.php
@@ -3,15 +3,26 @@
 	include('dbcon.php');
 	
     $sendID = $_POST["sendID"];
-    $recieveName = $_POST["recieveID"];
-	
-    $statement = mysqli_prepare($con, 'INSERT INTO friend(sender, reciever) VALUES(?, ?);');
-    mysqli_stmt_bind_param($statement, 'ss', $sendID, $recieveName);
-    mysqli_stmt_execute($statement);
-	
+    $recieveID = $_POST["recieveID"];
+
     $response = array();
 
-    $response["success"] = true;
+    $statement = mysqli_prepare($con, 'SELECT userID FROM user WHERE userID = ?');
+    mysqli_stmt_bind_param($statement, 's', $recieveID);
+    mysqli_stmt_execute($statement);
+        
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID);
+
+    $response["success"] = false;
+     
+    while(mysqli_stmt_fetch($statement)) {
+        $statement = mysqli_prepare($con, 'INSERT INTO friend(sender, reciever) VALUES(?, ?);');
+        mysqli_stmt_bind_param($statement, 'ss', $sendID, $recieveID);
+        mysqli_stmt_execute($statement);
+                
+        $response["success"] = true;
+    }
+
     echo json_encode($response);
-?>
 ?>
