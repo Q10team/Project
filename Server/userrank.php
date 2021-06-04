177a0713e28c0c362b00bded5a3688cc9f161b3e
황규현 <57509844+hgyuhyeon@users.noreply.github.com>
2021-06-04 21:17:17 +0900

    Update userrank.php

--- Server/userrank.php
+++ Server/userrank.php
@@ -1,22 +1,19 @@
 <?php
 	
 	include('dbcon.php');
-   
-    $ID = $_POST["userID"];
-	
-    $statement = mysqli_prepare($con, 'SELECT userName, userPoint FROM user WHERE userID = ?');
-    mysqli_stmt_bind_param($statement, 's', $tempresponse['userID']);
+   	
+    $statement = mysqli_prepare($con, 'SELECT userName, userPoint FROM user');
     mysqli_stmt_execute($statement);
 	
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $userPoint);
+    mysqli_stmt_bind_result($statement, $userName, $userPoint);
  
 
     $response = array();
     $total_response = array();
 
     while(mysqli_stmt_fetch($statement)) {
-        $response['name'] = $userID;
+        $response['name'] = $userName;
         $response['point'] = $userPoint;
         $total_response[] = $response;
     }
